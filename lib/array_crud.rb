bf7633731766ebc14e94e007bf8778c6c622006f
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-23 16:15:21 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,7 +33,7 @@ end
 
 def retrieve_first_element_from_array(array)
   goldfishes = ["wow", "One", "am", "Three"]
-  golsfishes.first()
+  goldfishes.first()
 end
 
 def retrieve_last_element_from_array(array)
