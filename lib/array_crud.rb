5f50ace47efef09fb5dab0c050df208f53dbb7d2
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-23 16:12:14 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   goldfishes = ["Maurice", "One", "Two", "Three"]
-  goldfishes.pop
+  goldfishes.pop()
 end
 
 def remove_element_from_start_of_array(array)
