a1935ebfddc47915789fa1e35a95a4076c5f2880
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-23 16:11:58 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-
+  goldfishes = ["Maurice", "One", "Two", "Three"]
+  goldfishes.pop
 end
 
 def remove_element_from_start_of_array(array)
