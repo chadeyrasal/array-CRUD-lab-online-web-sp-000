d3a1cd3b432ca5a897f6b16bd9793ecc402277f8
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-23 16:09:03 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   goldfishes = ["Maurice", "One", "Two", "Three"]
-  goldfishes << "Baurice"
+  goldfishes << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
